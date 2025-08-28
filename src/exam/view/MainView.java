64df621d042b81c3bd3eba1a02fbb149e610671e
BongBong <8253jang@daum.net>
2025-08-28 16:51:42 +0900

    Update MainView.java

--- src/exam/view/MainView.java
+++ src/exam/view/MainView.java
@@ -5,6 +5,7 @@ public class MainView {
 	public static void main(String[] args) {
 		System.out.println("시작합니다.~~^^");
         System.out.println("배고파요");
+		System.out.println("pullRequest 테스를 위한 원격 수정~");
 	}
 
 }
