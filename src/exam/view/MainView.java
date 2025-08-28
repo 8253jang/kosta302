dce6b96e5e4bb450b9506f2d5c03cd0fcd726dc1
BongBong <8253jang@daum.net>
2025-08-28 14:34:56 +0900

    수정 Revert "Main수정함"
    This reverts commit 627b883315a4e6a85583e2f0e96c4256e244a9d8.

--- src/exam/view/MainView.java
+++ src/exam/view/MainView.java
@@ -5,7 +5,6 @@ public class MainView {
 	public static void main(String[] args) {
 		System.out.println("시작합니다.~~^^");
         System.out.println("배고파요");
-        System.out.println("로컬에서 수정함~~");
 	}
 
 }
