627b883315a4e6a85583e2f0e96c4256e244a9d8
BongBong <8253jang@daum.net>
2025-08-28 14:27:33 +0900

    Main수정함

--- src/exam/view/MainView.java
+++ src/exam/view/MainView.java
@@ -5,6 +5,7 @@ public class MainView {
 	public static void main(String[] args) {
 		System.out.println("시작합니다.~~^^");
         System.out.println("배고파요");
+        System.out.println("로컬에서 수정함~~");
 	}
 
 }
