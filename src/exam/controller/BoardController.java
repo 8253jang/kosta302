d3635c0c6001507912fe8e581d52fdd4c594d6f3
BongBong <8253jang@daum.net>
2025-08-28 15:43:42 +0900

    완료했엉ㅅ~

--- src/exam/controller/BoardController.java
+++ src/exam/controller/BoardController.java
@@ -9,6 +9,10 @@ public class BoardController {
   public void insert() {
      System.out.println("서버에서 작업함~");
   }
+  
+  public void update() {
+	  System.out.println("로컬에서 updat입니다.~");
+  }
 
 	public void  delete() {
      System.out.println("서버에서  삭제 추가~");
